cd201f13d4fd3f0edd450c3e9fab613c329f2c36
Bartek <ebartosz.d@gmail.com>
2019-05-12 23:00:51 +0200

    #9 refactored existing interfaces to generic crud interfaces

--- pet-clinic-data/src/main/java/com/petclinicmilo/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/petclinicmilo/petclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.petclinicmilo.petclinic.services;
 
-import com.petclinicmilo.petclinic.model.Owner;
 import com.petclinicmilo.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet  findById(Long id);
-
-    Vet save ( Owner owner);
-
-    Set<Vet> findAll();
 }
